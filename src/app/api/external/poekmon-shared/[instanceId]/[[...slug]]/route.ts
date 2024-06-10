f1501ef05059407526420dd2e4487f191bf85549
moeakwak <moeakwak@gmail.com>
2024-06-10 22:45:43 +0800

    fix: remove router cache

--- src/app/api/external/poekmon-shared/[instanceId]/[[...slug]]/route.ts
+++ src/app/api/external/poekmon-shared/[instanceId]/[[...slug]]/route.ts
@@ -1,40 +1,33 @@
 import { NextResponse, type NextRequest } from "next/server";
-import { type Router, createAuthMiddleware, createRouter, extendRouter, setRouterContext } from "./hono-router";
+import { createAuthMiddleware, createRouter, extendRouter, setRouterContext } from "./hono-router";
 import { db } from "@/server/db";
 import { and, eq } from "drizzle-orm";
 import { serviceInstances } from "@/server/db/schema";
 import { type PoekmonSharedInstanceData } from "@/schema/service/poekmon-shared.schema";
-
-const ROUTERS: Record<string, Router> = {};
 
 const handler = async (req: NextRequest, { params }: { params: { instanceId: string; slug: string[] } }) => {
   const { instanceId, slug } = params;
 
   const basePath = `/api/external/poekmon-shared/${params.instanceId}`;
 
-  if (ROUTERS[instanceId] === undefined) {
-    const instance = await db.query.serviceInstances.findFirst({
-      where: and(eq(serviceInstances.id, instanceId), eq(serviceInstances.type, "POEKMON_SHARED")),
-    });
-    if (!instance) {
-      return NextResponse.json({ detail: "Instance Not Found" }, { status: 404 });
-    }
-
-    const instanceData = instance.data as PoekmonSharedInstanceData;
-
-    const router = createRouter(basePath);
-    router.use(createAuthMiddleware(instanceData.secret));
-
-    setRouterContext(router, {
-      instanceId,
-      instanceData,
-    });
-
-    extendRouter(router);
-    ROUTERS[instanceId] = router;
+  const instance = await db.query.serviceInstances.findFirst({
+    where: and(eq(serviceInstances.id, instanceId), eq(serviceInstances.type, "POEKMON_SHARED")),
+  });
+  if (!instance) {
+    return NextResponse.json({ detail: "Instance Not Found" }, { status: 404 });
   }
 
-  const router = ROUTERS[instanceId]!;
+  const instanceData = instance.data as PoekmonSharedInstanceData;
+
+  const router = createRouter(basePath);
+  router.use(createAuthMiddleware(instanceData.secret));
+
+  setRouterContext(router, {
+    instanceId,
+    instanceData,
+  });
+
+  extendRouter(router);
 
   return await router.fetch(req);
 };
